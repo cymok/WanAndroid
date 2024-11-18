cbf45a816bb54b1d7d9b16ae948e4361fec2e7cc
mok <mzx0766@gmail.com>
2024-11-18 08:12:14 +0800

    完善加密解密方法

--- app/src/main/java/com/example/wan/android/utils/SerializableUtils.kt
+++ app/src/main/java/com/example/wan/android/utils/SerializableUtils.kt
@@ -3,10 +3,10 @@ package com.example.wan.android.utils
 import com.blankj.utilcode.util.ConvertUtils
 import java.io.Serializable
 
-fun Serializable.toByteArray(): ByteArray {
+fun Serializable.serializable2ByteArray(): ByteArray {
     return ConvertUtils.serializable2Bytes(this)
 }
 
-fun ByteArray.toObject(): Any {
+fun ByteArray.byteArray2Serializable(): Any {
     return ConvertUtils.bytes2Object(this)
 }
